a4231d318eafeed1f99a716482a3cf22208f4a7b
Vinique <ikehalex@gmail.com>
2025-06-24 05:48:56 +0100

    Hid config info in .env variable

--- material-tracker-react/src/firebase.js
+++ material-tracker-react/src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNZgou-GZMD-ajy_HywunRD3-JdntRv1I",
-  authDomain: "sitsl-inventory-tracker.firebaseapp.com",
-  projectId: "sitsl-inventory-tracker",
-  storageBucket: "sitsl-inventory-tracker.firebasestorage.app",
-  messagingSenderId: "238816182378",
-  appId: "1:238816182378:web:7070e7eadb9b4ee7b3ca13"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
